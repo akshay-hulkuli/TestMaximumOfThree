8bb0985c51eb79dc1a650691933b9ec772b11499
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-29 20:43:01 +0530

    REFACTORED:generic method to take multiple data types

--- src/com/bridgelabz/testmaximum/TestMaximumOfThree.java
+++ src/com/bridgelabz/testmaximum/TestMaximumOfThree.java
@@ -4,27 +4,47 @@ import java.util.Scanner;
 
 public class TestMaximumOfThree {
 	
-	private static String findMax(String str1, String str2, String str3) {
-		String max = str1;
+	private static <E extends Comparable<E>> E findMax(E data1, E data2, E data3) {
+		E max = data1;
 		
-		if(str2.compareTo(max)>0) {
-			max = str2;
+		if(data2.compareTo(max)>0) {
+			max = data2;
 		}
-		if(str3.compareTo(max)>0) {
-			max = str3;
+		if(data3.compareTo(max)>0) {
+			max = data3;
 		}
 		return max;
 	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter three strings");
-		String str1 = sc.next();
-		String str2 = sc.next();
-		String str3 = sc.next();
 		
-		String max = findMax(str1 , str2, str3);
+		boolean exit = true;
+		while(true) {
+			System.out.println("Select options 1: compare integers 2: compare floating point numbers 3:copmare strings 4:exit");
+			switch(sc.nextInt()) {
+				case 1:
+					System.out.println("Enter 3 integers");
+					int maxInt = findMax(sc.nextInt(),sc.nextInt(),sc.nextInt());
+					System.out.println("the maximum integer is :"+maxInt);
+					break;
+				case 2:
+					System.out.println("Enter 3 Floating point numbers");
+					Float maxFloat = findMax(sc.nextFloat(),sc.nextFloat(),sc.nextFloat());
+					System.out.println("the maximum Floating point numbers is :"+maxFloat);
+					break;
+				case 3:
+					System.out.println("Enter 3 strings");
+					String maxString = findMax(sc.next(),sc.next(),sc.next());
+					System.out.println("the largest string based on compareTo is :"+maxString);
+					break;
+				default:
+					exit = false;
+					System.out.println("Exiting");
+				
+			}
+		}
+	
 		
-		System.out.println("the maximum string is : "+max);
 	}
 }
